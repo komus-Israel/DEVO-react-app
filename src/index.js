070d2468896c8f4368e7a8028a89dd2cb0970f9c
komus-Israel <komolehinIsrael@gmail.com>
2021-12-20 23:05:33 +0100

    commented out redux dev tool

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 
 const store = createStore(
     allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 
